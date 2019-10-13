043f83647a898d3d2c1c5891b70f2699bfa494c0
K1ngArtes <arthurslife@ymail.com>
2019-10-13 12:38:22 +0100

    Add background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 import { Form, Container } from 'react-bootstrap';
 
 function App() {
+
     return (
-        <div className="App">
+
+            <LandingPage />
+    );
+}
+
+const LandingPage = () => {
+    const backgroundImageSrc1 = "/res/forest-background.jpg";
+    const backgroundImageSrc2 = "/res/forest-helicopter.jpg";
+
+    return (
+        <div style={{
+            background: 'url(' + backgroundImageSrc2 + ')',
+            height:'100%',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }}>
             <Container style={{
                 position: 'fixed',
                 top: '50%',
